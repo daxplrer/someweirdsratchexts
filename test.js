8340b7c7e2e69f29f6bc3502952dced9d944cdd0
daxplrer <70825723+daxplrer@users.noreply.github.com>
2024-03-07 20:10:54 +0700

    Create test.js
    for testing purposes

---
+++ test.js
@@ -0,0 +1,28 @@
+(function(Scratch) {
+  'use strict';
+
+  if (!Scratch.extensions.unsandboxed) {
+    throw new Error('This Hello World example must run unsandboxed');
+  }
+
+  class HelloWorld {
+    getInfo() {
+      return {
+        id: 'helloworldunsandboxed',
+        name: 'Unsandboxed Hello World',
+        blocks: [
+          {
+            opcode: 'hello',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'Hello!'
+          }
+        ]
+      };
+    }
+    hello(ars, utils) {
+      console.log(args, utils)
+      return 'World!';
+    }
+  }
+  Scratch.extensions.register(new HelloWorld());
+})(Scratch);
