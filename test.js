e631486685f4ae9137d2a8a85c38a0bf580c0d6d
daxplrer <70825723+daxplrer@users.noreply.github.com>
2024-03-07 21:02:28 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,28 +1,76 @@
-(function(Scratch) {
-  'use strict';
-
-  if (!Scratch.extensions.unsandboxed) {
-    throw new Error('This Hello World example must run unsandboxed');
-  }
-
-  class HelloWorld {
-    getInfo() {
-      return {
-        id: 'helloworldunsandboxed',
-        name: 'Unsandboxed Hello World',
-        blocks: [
-          {
-            opcode: 'hello',
-            blockType: Scratch.BlockType.REPORTER,
-            text: 'Hello!'
-          }
-        ]
-      };
+var __OPENNOTIFY_SCRATCHEXT__DEFAULTCONF = {
+    point: 'http://open-notify.org',
+    fdatas:{
+        stationloc:undefined,
+        astronauts:undefined
     }
-    hello(ars, utils) {
-      console.log(args, utils)
-      return 'World!';
+}
+Object.freeze(__OPENNOTIFY_SCRATCHEXT__DEFAULTCONF);
+(function (Scratch) {
+    'use strict';
+    const vm = Scratch.vm;
+    const rt = vm.runtime
+    let apistore = __OPENNOTIFY_SCRATCHEXT__DEFAULTCONF
+    if (!Scratch.extensions.unsandboxed) {
+        throw new Error('run this on unsandboxed mode and make sure you are loading it in turbowarp-based scratch');
     }
-  }
-  Scratch.extensions.register(new HelloWorld());
-})(Scratch);
+    
+
+    class AstronautsApi {
+        constructor(){
+            rt.on('BEFORE_EXECUTE', ()=>apistore = __OPENNOTIFY_SCRATCHEXT__DEFAULTCONF)
+        }
+        getInfo() {
+            return {
+                id: 'opennotifyapi',
+                name: 'Open Notify Astronauts Api',
+                blocks: [
+                    {
+                        opcode: 'fetchstationloc',
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: 'Fetch station location'
+                    },
+                    {
+                        opcode: 'setwebpoint',
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: 'Set web starting point to',
+                        arguments: {
+                            SPOINT: {
+                              type: Scratch.ArgumentType.STRING,
+                              menu: 'Starting point'
+                            }
+                        }
+                    },
+                    {
+                        opcode: 'resvar',
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: "Reset open notify extension's cache"
+                    },
+                    {
+                        opcode: 'rawstationloc',
+                        blockType: Scratch.BlockType.REPORTER,
+                        text: "Get raw station data"
+                    },
+                ]
+            };
+        }
+        resvar(){
+            apistore = __OPENNOTIFY_SCRATCHEXT__DEFAULTCONF
+        }
+        setwebpoint(args, utils){
+            apistore.point = args.SPOINT.toString()
+        }
+        fetchstationloc() {
+            let fdata = {success:false}
+            try {
+          if (Scratch.canFetch(`${apistore.point}/iss-now.json`)) fetch(`${apistore.point}/iss-now.json`).then((res)=>fdata = res.json())
+            } finally{}
+          apistore.fdatas.stationloc = fdata
+        }
+        rawstationloc(){
+            return apistore.fdatas.stationloc||{success:false}
+        }
+        
+    } 
+    Scratch.extensions.register(new AstronautsApi());
+})
